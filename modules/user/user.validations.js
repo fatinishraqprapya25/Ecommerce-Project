a0d35af878e78480679959bf0e89f92602157091
unknown <dev.prapya@gmail.com>
2024-10-20 19:05:25 +0600

    login validation schema done

--- modules/user/user.validations.js
+++ modules/user/user.validations.js
@@ -22,5 +22,16 @@ userValidations.userRegistrationValidationSchema = z.object({
     }
 });
 
+userValidations.loginValidationSchema = z.object({
+    body: z.object({
+        email: z.string({
+            required_error: "Email is required",
+        }).email("Invalid email address"),
+
+        password: z.string({
+            required_error: "Password is required",
+        }).min(6, "Password must be at least 6 characters"),
+    })
+});
 
 module.exports = userValidations;
