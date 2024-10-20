1e1e4bfc2fa065b78d22e2cda1c92de5aee5ee17
unknown <dev.prapya@gmail.com>
2024-10-20 18:57:46 +0600

    login service done

--- modules/user/user.service.js
+++ modules/user/user.service.js
@@ -1,10 +1,27 @@
 const User = require("./user.model");
+const bcrypt = require("bcrypt");
 
 const userServices = {};
 
 userServices.createUser = async (userData) => {
     const user = new User(userData);
     return await user.save();
+}
+
+userServices.login = async ({ email, password }) => {
+    const user = await User.findOne({ email });
+    if (!user) throw new Error("Invalid email or password");
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) throw new Error("Invalid email or password");
+
+    const token = jwt.sign(
+        { id: user._id, role: user.role },
+        process.env.JWT_SECRET,
+        { expiresIn: "1d" }
+    );
+
+    return { token, user };
 }
 
 userServices.disableUser = async (userId) => {
