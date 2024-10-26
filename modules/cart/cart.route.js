405c212445b2c7e094d56d4eb15d8b4aaa4960b5
unknown <dev.prapya@gmail.com>
2024-10-26 13:09:39 +0600

    basic routing setup done

--- modules/cart/cart.route.js
+++ modules/cart/cart.route.js
@@ -1,6 +1,13 @@
 const { Router } = require("express");
+const cartController = require("./cart.controller");
+const checkLogin = require("../../middlewares/checkLogin");
 
 const cartRouter = Router();
 
+cartRouter.post("/", checkLogin, cartController.createOrUpdateCart);
+cartRouter.get("/", checkLogin, cartController.getCartByUserId);
+cartRouter.patch("/:productId", checkLogin, cartController.updateProductQuantity);
+cartRouter.delete("/:productId", checkLogin, cartController.removeProductFromCart);
+cartRouter.delete("/:productId", checkLogin, cartController.removeProductFromCart);
 
 module.exports = cartRouter;
