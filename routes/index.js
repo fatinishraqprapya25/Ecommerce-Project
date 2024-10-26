9cdab4fd80d560f06239de5cc9baaee16b52bc45
unknown <dev.prapya@gmail.com>
2024-10-26 13:03:56 +0600

    basic cartRouter Setup done

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const publicUser = require("../modules/user/user.route");
 const adminRoute = require("../modules/admin/admin.route");
 const productRouter = require("../modules/product/product.route");
+const cartRouter = require("../modules/cart/cart.route");
 
 const appRouter = Router();
 
@@ -9,7 +10,8 @@ const routes = [
     { path: "/auth", router: publicUser.authRoute },
     { path: "/user", router: publicUser.userRoute },
     { path: "/admin", router: adminRoute },
-    { path: "/products", router: productRouter }
+    { path: "/products", router: productRouter },
+    { path: "/cart", router: cartRouter },
 ]
 
 routes.forEach(route => appRouter.use(route.path, route.router));
