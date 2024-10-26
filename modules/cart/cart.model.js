e1aac749e43cad7badf558894d03e3e8d1a75b7c
unknown <dev.prapya@gmail.com>
2024-10-26 12:46:10 +0600

    Model for Cart done

---
+++ modules/cart/cart.model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const cartSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    products: [
+        {
+            productId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Product',
+                required: true
+            },
+            quantity: {
+                type: Number,
+                required: true,
+                min: 1
+            },
+            price: {
+                type: Number,
+                required: true,
+                min: 0
+            }
+        }
+    ]
+}, { timestamps: true });
+
+const Cart = mongoose.model('Cart', cartSchema);
+
+module.exports = Cart;
