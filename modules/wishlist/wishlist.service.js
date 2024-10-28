39ac7881870c77ba9ba6100b9fdeb6c30f60de1a
unknown <dev.prapya@gmail.com>
2024-10-28 10:56:03 +0600

    basic wishlist service done

---
+++ modules/wishlist/wishlist.service.js
@@ -0,0 +1,29 @@
+const Wishlist = require('../models/Wishlist');
+
+const wishlistService = {};
+
+wishlistService.getWishlistByUser = async (userId) => {
+    await Wishlist.findOne({ user: userId }).populate('products.product');
+}
+
+wishlistService.addProductToWishlist = async (userId, productId) => {
+    await Wishlist.findOneAndUpdate(
+        { user: userId },
+        { $addToSet: { products: { product: productId } } },
+        { new: true, upsert: true }
+    );
+}
+
+wishlistService.removeProductFromWishlist = async (userId, productId) => {
+    await Wishlist.findOneAndUpdate(
+        { user: Types.ObjectId(userId) },
+        { $pull: { products: { product: productId } } },
+        { new: true }
+    );
+}
+
+wishlistService.deleteWishlistByUser = async (userId) => {
+    await Wishlist.findOneAndDelete({ user: userId });
+}
+
+module.exports = wishlistService;
