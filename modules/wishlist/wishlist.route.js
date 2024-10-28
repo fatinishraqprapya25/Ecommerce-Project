2148496b7d5b7093180c805e8b7a43c1603dfc97
unknown <dev.prapya@gmail.com>
2024-10-28 11:05:43 +0600

    wishlist router setup done

--- modules/wishlist/wishlist.route.js
+++ modules/wishlist/wishlist.route.js
@@ -1,7 +1,12 @@
 const { Router } = require("express");
+const checkLogin = require("../../middlewares/checkLogin");
+const wishlistController = require("./wishlist.controller");
 
 const wishlistRouter = Router();
 
-
+wishlistRouter.post("/", checkLogin, wishlistController.addProductToWishlist);
+wishlistRouter.get("/", checkLogin, wishlistController.getWishlistByUser);
+wishlistRouter.delete("/:productId", checkLogin, wishlistController.removeProductFromWishlist);
+wishlistRouter.delete("/clear", checkLogin, wishlistController.clearWishlist);
 
 module.exports = wishlistRouter;
