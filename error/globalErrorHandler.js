e36c12c6132d850d4bad65a7b8bd075510f35df5
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-02-18 14:51:51 +0600

    global error handler optimized

--- error/globalErrorHandler.js
+++ error/globalErrorHandler.js
@@ -1,11 +1,29 @@
-const sendResponse = require("../utils/sendResponse")
+const sendResponse = require("../utils/sendResponse");
+const { ZodError } = require("zod");
 
 const globalErrorHandler = (error, req, res, next) => {
-    sendResponse(res, 500, {
+    let statusCode = 500;
+    let message = "Something went wrong";
+    let errorDetails = null;
+
+    if (error instanceof ZodError) {
+        statusCode = 400;
+        message = "Validation error";
+        errorDetails = error.issues.map(issue => ({
+            path: issue.path.join("."),
+            message: issue.message
+        }));
+    } else if (error.name === "ValidationError") {
+        statusCode = 400;
+        message = error.message;
+        errorDetails = error.errors;
+    }
+
+    sendResponse(res, statusCode, {
         success: false,
-        message: error.message,
-        error: error
+        message,
+        error: errorDetails || error.message
     });
-}
+};
 
 module.exports = globalErrorHandler;
