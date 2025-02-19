3e8f91b202a534083019f63c4ebc35e36f70752a
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-02-19 15:30:02 +0600

    trackTraffic Middleware done

--- middlewares/trackTraffic.js
+++ middlewares/trackTraffic.js
@@ -1,5 +1,15 @@
-// const trackTraffic = async (req, res, next) => {
+const isLoggedIn = require("../utils/isLoggedin");
 
-// }
+const trackTraffic = async (req, res, next) => {
+    const isLoggedIn = isLoggedIn(req);
+    const userType = isLoggedIn ? "authenticated" : "general";
+    const userId = isLoggedIn ? req.user.id : req.ip;
+    const trafficData = {
+        userType,
+        userId,
+        route: req.originalUrl,
+        method: req.method
+    }
+}
 
-// module.exports = trackTraffic;
+module.exports = trackTraffic;
