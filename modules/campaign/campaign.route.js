1e57157fbaf2eb6ac540adcbeec3c0badf7ea1cc
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-02-21 15:05:23 +0600

    campaign image optimized

--- modules/campaign/campaign.route.js
+++ modules/campaign/campaign.route.js
@@ -8,7 +8,10 @@ const campaignMiddlewares = require("./campaign.middlewares");
 
 const campaignRouter = Router();
 
-campaignRouter.post("/", checkAdmin, upload("campaigns").array("images", 3), validateRequest(campaignValidations.createCampaign, campaignMiddlewares.deteteUploadedPhotoIfValidationFailed), campaignController.createCampaign);
+const CAMPAIGN_IMAGE_MAX_SIZE = 15;
+const CAMPAIGN_IMAGE_TYPES = /jpeg|jpg|png/;
+
+campaignRouter.post("/", checkAdmin, upload("campaigns", CAMPAIGN_IMAGE_MAX_SIZE, CAMPAIGN_IMAGE_TYPES).array("images", 3), validateRequest(campaignValidations.createCampaign, campaignMiddlewares.deteteUploadedPhotoIfValidationFailed), campaignController.createCampaign);
 campaignRouter.get("/", checkAdmin, campaignController.getAllCampaigns);
 campaignRouter.get("/:id", checkAdmin, campaignController.getCampaignById);
 campaignRouter.get("/:id", checkAdmin, campaignController.getCampaignById);
