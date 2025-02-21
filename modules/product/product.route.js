7c70783bd8fed638a4dbcfeca7522093dfedebef
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-02-21 20:08:58 +0600

    product route updated

--- modules/product/product.route.js
+++ modules/product/product.route.js
@@ -8,8 +8,11 @@ const productMiddlewares = require("./product.middlewares");
 
 const productRouter = Router();
 
+const PRODUCT_IMG_MAX_SIZE = 15;
+const PRODUCT_IMG_TYPES = /jpeg|jpg|png/;
+
 // admin requests
-productRouter.post("/", varifyAdmin, upload("products").array("images", 3), validateRequest(productValidations.createSchema, productMiddlewares.deteteUploadedPhotoIfValidationFailed), productController.createProduct);
+productRouter.post("/", varifyAdmin, upload("products", PRODUCT_IMG_MAX_SIZE, PRODUCT_IMG_TYPES).array("images", 3), validateRequest(productValidations.createSchema, productMiddlewares.deteteUploadedPhotoIfValidationFailed), productController.createProduct);
 productRouter.patch("/:id", varifyAdmin, productController.updateProduct);
 productRouter.delete("/:id", varifyAdmin, productController.deleteProduct);
 
