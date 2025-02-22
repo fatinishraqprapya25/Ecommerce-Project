d55fb301dc4a51edbbeff511ca196e1eb4cab477
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-02-22 21:04:20 +0600

    products valition optimized

--- modules/order/order.validation.js
+++ modules/order/order.validation.js
@@ -1,13 +1,18 @@
 const { z } = require('zod');
 
-const orderValidatdions = {};
+const orderValidations = {};
 
-orderValidatdions.createOrder = z.object({
+orderValidations.createOrder = z.object({
     body: z.object({
         address: z.string().nonempty("Address is required"),
         paymentMethod: z.enum(['cash_on_delivery', 'bkash', 'nagad']),
+        products: z.array(
+            z.object({
+                productId: z.string().nonempty("Product ID is required"),
+                quantity: z.number().int().positive("Quantity must be a positive integer")
+            })
+        ).min(1, "At least one product is required"),
     })
 });
 
-
-module.exports = orderValidatdions;
+module.exports = orderValidations;
