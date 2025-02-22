e91ace1d816fa15989b081133f4310aa9bba13ad
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-02-22 22:55:11 +0600

    admin router optimized

--- modules/admin/admin.route.js
+++ modules/admin/admin.route.js
@@ -14,4 +14,8 @@ adminRoute.delete("/:id", checkAdmin, adminController.removeAdmin);
 adminRoute.post("/enable/:id", checkAdmin, adminController.enableUser);
 adminRoute.post("/disable/:id", checkAdmin, adminController.disableUser);
 
+adminRoute.get("/traffic/today", checkAdmin, adminController.getTodaysTraffic);
+adminRoute.get("/traffic/thismonth", checkAdmin, adminController.getThisMonthsTraffic);
+adminRoute.get("/traffic/thisyear", checkAdmin, adminController.getThisYearsTraffic);
+
 module.exports = adminRoute;
