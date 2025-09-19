86fa9b21132cbfdc9cbfe23f479426f5e42d10f7
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-09-19 00:54:48 -0700

    campaign router optimized

--- modules/campaign/campaign.route.js
+++ modules/campaign/campaign.route.js
@@ -4,17 +4,21 @@ const campaignController = require("./campaign.controller");
 const campaignValidations = require("./campaign.validation");
 const checkAdmin = require("../../middlewares/checkAdmin");
 const upload = require("../../utils/upload");
-const campaignMiddlewares = require("./campaign.middlewares");
+const deleteCloudinaryFile = require("../../utils/deleteUploadedFile");
 
 const campaignRouter = Router();
 
 const CAMPAIGN_IMAGE_MAX_SIZE = 15;
 const CAMPAIGN_IMAGE_TYPES = /jpeg|jpg|png/;
 
-campaignRouter.post("/", checkAdmin, upload("campaigns", CAMPAIGN_IMAGE_MAX_SIZE, CAMPAIGN_IMAGE_TYPES).array("images", 3), validateRequest(campaignValidations.createCampaign, campaignMiddlewares.deteteUploadedPhotoIfValidationFailed), campaignController.createCampaign);
+campaignRouter.post("/", checkAdmin, upload("campaigns", CAMPAIGN_IMAGE_MAX_SIZE, CAMPAIGN_IMAGE_TYPES).array("images", 3), validateRequest(campaignValidations.createCampaign, deleteCloudinaryFile), campaignController.createCampaign);
+
 campaignRouter.get("/", checkAdmin, campaignController.getAllCampaigns);
+
 campaignRouter.get("/active", checkAdmin, campaignController.getAllActiveCampaigns);
+
 campaignRouter.get("/:id", checkAdmin, campaignController.getCampaignById);
+
 campaignRouter.patch("/:id", checkAdmin, campaignController.updateCampaign);
 
 module.exports = campaignRouter;
