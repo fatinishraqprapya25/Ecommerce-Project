015783d8e8856452baf29ab0c4f57e6a9bf08112
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-09-19 06:55:53 -0700

    order router optimized

--- modules/order/order.route.js
+++ modules/order/order.route.js
@@ -8,10 +8,16 @@ const orderValidatdions = require("./order.validation");
 const orderRouter = Router();
 
 orderRouter.post("/checkout", validateRequest(orderValidatdions.createOrder), checkLogin, orderController.createOrder);
+
 orderRouter.get("/", checkLogin, orderController.getOrdersByUser);
+
 orderRouter.get("/:orderId", checkLogin, orderController.getOrderById);
+
 orderRouter.patch("/:orderId", checkLogin, orderController.cancelOrder);
+
+// admin routes
 orderRouter.post("/status", validateRequest(orderValidatdions.changeOrderStatus), checkAdmin, orderController.changeOrderStatus);
+
 orderRouter.get("/get/all", checkAdmin, orderController.getAllOrders);
 
 module.exports = orderRouter;
